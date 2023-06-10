42f19452d6f2e85ba66db931af924fd5d4e33646
metastable-void <git@yuka.h09.eu>
2023-06-10 18:00:02 +0900

    updated to more robust settings

--- srv/enable-policy-base-routing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-IP=/sbin/ip
-"$IP" route del 10.194.1.1 dev eth0 table docomo-route >/dev/null 2>&1 || true
-"$IP" rule del to 10.194.1.1 lookup docomo-route >/dev/null 2>&1 || true
-
-"$IP" route add 10.194.1.1 dev eth0 table docomo-route
-"$IP" rule add to 10.194.1.1 lookup docomo-route
